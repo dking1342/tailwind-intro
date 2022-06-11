3c2bce0ed55dc55bf3f31de125d2f45bba02e577
David <leanmachine42@yahoo.com>
2022-06-11 18:40:33 -0500

    creating tailwind classes in the html, in styles.css with @apply. edit tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,12 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {},
+    container: {
+      center:true,
+      padding:"2rem"
+    }
   },
   plugins: [],
 }
